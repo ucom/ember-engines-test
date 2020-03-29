7fcd17830769ecdf8d716bea6b62b54f6274610b
Tilo Hirsch <info@unternehmen.com>
2020-03-29 16:56:49 -0400

    first commit

---
+++ app/components/test-image.js
@@ -0,0 +1,14 @@
+import Component from '@ember/component';
+// import {RandomPicture} from 'random-picture';
+
+// async function getPicture(){
+//   const image = await RandomPicture();
+//   console.log('image from module received', image);
+//   return image;
+// }
+// getPicture().then( (image) => console.log(image) );
+// import {a} from 'sass';
+
+export default Component.extend({
+  a: 'image'
+});
